ad2c1012e53ebbd31b1fff1d73d4abf03cc4e25e
varchasvipandey <varchasvip@gmail.com>
2021-08-05 21:11:22 +0530

    modified section scroll function

--- src/components/shared/organisms/ContentTray/ContentTray.tsx
+++ src/components/shared/organisms/ContentTray/ContentTray.tsx
@@ -7,27 +7,15 @@ interface ContentTrayProps {
   contentList: QueryNode[];
 }
 
-interface HTMLChildNode extends ChildNode {
-  offsetWidth?: number;
-}
-
-const elementWidth = (el: HTMLChildNode): any => {
-  return el.offsetWidth;
-};
-
 const ContentTray: React.FC<ContentTrayProps> = ({ contentList = [] }) => {
   const containerRef = React.useRef<HTMLDivElement>(null);
 
   const handleScroll = (direction: string): void => {
     const container = containerRef.current;
     if (container) {
-      const firstContent = container?.firstChild;
-      if (firstContent) {
-        let scrollXBy = elementWidth(firstContent);
-
-        if (direction === 'left') scrollXBy *= -1;
-        container.scrollBy({ left: scrollXBy, behavior: 'smooth' });
-      }
+      let scrollXBy = container.offsetWidth;
+      if (direction === 'left') scrollXBy *= -1;
+      container.scrollBy({ left: scrollXBy, behavior: 'smooth' });
     }
   };
 
