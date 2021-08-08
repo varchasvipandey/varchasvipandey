e3584933b51b12b06bac563b3123aa2950bc954e
varchasvipandey <varchasvip@gmail.com>
2021-08-08 20:43:30 +0530

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,19 @@
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          `G-7B2MEZG2FR`, // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     'gatsby-plugin-typescript',
     'gatsby-plugin-sass',
     `gatsby-plugin-react-helmet`,
@@ -48,14 +62,6 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_ID,
-        head: true,
-        enableWebVitalsTracking: true,
       },
     },
   ],
