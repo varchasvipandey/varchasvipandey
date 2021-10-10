e553ad09a57d5a090c38d16e2242750e6a40d6fe
varchasvipandey <varchasvip@gmail.com>
2021-10-10 20:05:43 +0530

    menu cross icon from img to react svg

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export * from './IconInterface';
 export { default as ShareIcon } from './ShareIcon';
 export { default as HeartIcon } from './HeartIcon';
+export { default as CrossIcon } from './CrossIcon';
